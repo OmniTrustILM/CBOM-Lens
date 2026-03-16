241ce8f568f71aa9e263a9e8a3490c069ff438bd
Michal Vyskocil <michal.vyskocil@3key.company>
2026-03-16 16:22:59 +0100

    remove buf[:] - not needed with ReadAll

--- internal/service/scan.go
+++ internal/service/scan.go
@@ -115,6 +115,8 @@ func (s *Scan) scan(ctx context.Context, entry model.Entry) ([]model.Detection, 
 	bp := s.pool.Get().(*[]byte)
 	buf := *bp
 	clear(buf)
+	// FIXME: memory from the pool is no longer used
+	// fix it or remove the pool in the future
 	buf, err = io.ReadAll(f)
 	if err != nil {
 		s.counter.IncErrFiles()
@@ -126,9 +128,6 @@ func (s *Scan) scan(ctx context.Context, entry model.Entry) ([]model.Detection, 
 		s.poolPutCounter.Add(1)
 		s.pool.Put(bp)
 	}()
-	// IMPORTANT: data must be passed as buf[:n] otherwise data from a previous
-	// file will be passed in
-	buf = buf[:len(buf)]
 
 	var detectionErrors []error
 	res := make([]model.Detection, 0, 10)
